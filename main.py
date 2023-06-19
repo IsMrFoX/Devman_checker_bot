4c1db2e2e4a0aa545a05a15c3c2a683bc19a5de2
Rostislav <ylrostik@mail.ru>
2023-06-19 18:36:16 +0300

    Fixed sending logs to telegram

--- main.py
+++ main.py
@@ -16,8 +16,9 @@ class TelegramLogsHandler(logging.Handler):
         self.chat_id = chat_id
 
     def emit(self, record):
-        log_entry = self.format(record)
-        self.bot.send_message(chat_id=self.chat_id, text=log_entry)
+        if record.levelno >= logging.ERROR:
+            log_entry = self.format(record)
+            self.bot.send_message(chat_id=self.chat_id, text=log_entry)
 
 
 def main():
